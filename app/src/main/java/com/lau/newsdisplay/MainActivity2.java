a2856e0991e42a6172c8f3299c9ae430b97b6827
anthonyykh <anthonykh01.ak@gmail.com>
2022-10-02 20:49:06 +0300

    added shared preference and added intent to redirect to news page

--- app/src/main/java/com/lau/newsdisplay/MainActivity2.java
+++ app/src/main/java/com/lau/newsdisplay/MainActivity2.java
@@ -2,14 +2,36 @@ package com.lau.newsdisplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity2 extends AppCompatActivity {
-
+    SharedPreferences shared;
+    EditText username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        SharedPreferences shared = this.getSharedPreferences("com.lau.shared", Context.MODE_PRIVATE);
+
     }
+
+    public void submit(View v){
+        username =findViewById(R.id.name);
+
+        shared.edit().putString("username", username.getText().toString()).apply();
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+        startActivity(intent);
+
+
+    }
+
 }
 
